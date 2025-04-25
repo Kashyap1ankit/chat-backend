f4da7ee4449cfe2a64bbe4fd1b09cdcf406c5630
Ankit <kashyap25ankit@gmail.com>
2025-04-26 02:18:29 +0530

    added backed the normal expiration timee

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,7 @@ app.use(
 app.use(
   expressSession({
     cookie: {
-      // maxAge: 7 * 24 * 60 * 60 * 1000,
-      maxAge: 20 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
     secret: "hello world",
     resave: false,
